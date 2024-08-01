a22776a62dfb7488c9e70f702c23d4b5829cfb71
called_D <called.0xd@gmail.com>
2024-08-01 22:11:46 +0900

    refactor: file name data date

--- plot-today.sh
+++ plot-today.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-data="logs/$(date +%F)"
+today="$(date +%F)"
+file="logs/$today"
 gnuplot << EOF
   set term x11 size 1300,600
-  set title "${data} co2 and temperature"
+  set title "co2 and temperature | $today"
 
   set xdata time
   set timefmt "%s"
@@ -21,8 +22,8 @@ gnuplot << EOF
   set arrow from graph 0,second 28 to graph 1,second 28 nohead lc rgb "red" dt '-'
 
   while (1) {
-    plot '${data}' using (\$1+(9*3600)):2 axis x1y1 with lines linewidth 2 title "co2"
-    replot '${data}' using (\$1+(9*3600)):3 axis x1y2 with lines title "temperature"
+    plot '${file}' using (\$1+(9*3600)):2 axis x1y1 with lines linewidth 2 title "co2"
+    replot '${file}' using (\$1+(9*3600)):3 axis x1y2 with lines title "temperature"
     pause 30
   }
 EOF
